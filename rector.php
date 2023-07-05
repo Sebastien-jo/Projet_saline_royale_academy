52dce60ebbc911e2ac57330cee11a72fbfc632f9
LeJo <jorgesebastien@gmail.com>
2023-07-05 12:54:40 +0200

    feat(ci): improve ci for deployment

--- rector.php
+++ rector.php
@@ -4,18 +4,30 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/app',
+        __DIR__ . 'app/back/src',
+        __DIR__ . 'app/back/tests',
     ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_82
-    //    ]);
+    $rectorConfig->sets([
+        SymfonyLevelSetList::UP_TO_SYMFONY_62,
+        LevelSetList::UP_TO_PHP_82,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+    ]);
+
+    // exclude rules
+    $rectorConfig->skip([
+        RemoveUnusedPrivatePropertyRector::class,
+    ]);
 };
