dad1285bd4e2dadcd806c0e9735c73c16e41514f
Teddy Gamiette <tgamiette@kernix.com>
2023-07-11 09:55:50 +0200

    feat(Entity): PFR-58 - Endpoint Forum Entity

---
+++ app/back/src/State/SetUserProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\DeleteOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Forum;
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class SetUserProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private readonly ProcessorInterface $persistProcessor,
+        private readonly ProcessorInterface $removeProcessor,
+        private readonly Security $security
+    ) {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        /** @var Forum $forum */
+        $forum = $data;
+
+        /** @var User $user */
+        $user = $this->security->getUser();
+        $forum->setUser($user);
+
+        if ($operation instanceof DeleteOperationInterface) {
+            return $this->removeProcessor->process($forum, $operation, $uriVariables, $context);
+        }
+
+        return $this->persistProcessor->process($forum, $operation, $uriVariables, $context);
+    }
+}
