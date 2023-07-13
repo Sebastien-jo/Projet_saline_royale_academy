1d35e7d0087ccb61796a2dcfcef95f30860fdb8c
Maélys CHASSIN <m.chassin@altelis.com>
2023-07-13 18:48:10 +0200

    fix(api token): fix use of token for api call

--- app/front/src/store/Slice/authSlice.js
+++ app/front/src/store/Slice/authSlice.js
@@ -13,11 +13,13 @@ const authSlice = createSlice({
             state.isAuthenticated = true;
             state.user = action.payload.user;
             state.token = action.payload.token;
+            window.localStorage.setItem('token', action.payload.token);
         },
         logout(state) {
             state.isAuthenticated = false;
             state.user = null;
             state.token = null;
+            window.localStorage.removeItem('token');
         },
     },
 });
