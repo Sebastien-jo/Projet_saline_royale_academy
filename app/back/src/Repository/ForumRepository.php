cb166d72c81b9d18889e5106bbbd12cc0eaa7cc5
Teddy Gamiette <tgamiette@kernix.com>
2023-08-23 12:36:29 +0200

    feat (Entity): PFR-95 - add numbers response to forum entity PFR-94 - add isLiked param to forum entity

--- app/back/src/Repository/ForumRepository.php
+++ app/back/src/Repository/ForumRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Forum;
+use App\Entity\Like;
+use App\Entity\Masterclass;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -37,5 +40,40 @@ class ForumRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+    /**
+     * @return array<Masterclass>
+     */
+    public function findAllLiked(int $userId): array
+    {
+        $dql = '
+            SELECT f 
+            FROM ' . Forum::class . ' f
+            INNER JOIN ' . Like::class . ' l WITH f.id = l.forum AND l.user = :userId';
+
+        return $this->getEntityManager()->createQuery($dql)
+                ->setParameter('userId', $userId)
+                ->getResult()
+        ;
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function findLiked(int $id, int $userId): ?Forum
+    {
+        $dql = '
+            SELECT f
+            FROM ' . Forum::class . ' f
+            INNER JOIN ' . Like::class . ' l WITH f.id = l.forum AND l.user = :userId
+            WHERE f.id = :id
+        ';
+
+        return $this->getEntityManager()->createQuery($dql)
+                ->setParameter('userId', $userId)
+                ->setParameter('id', $id)
+                ->getOneOrNullResult()
+        ;
     }
 }
